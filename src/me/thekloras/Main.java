ab08c68cb04e11077e0771d26278ae96d6d692f3
PC <lala.karolis@gmail.com>
2021-10-11 10:37:46 +0300

    pakeitimai

--- src/me/thekloras/Main.java
+++ src/me/thekloras/Main.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Main {
 
+    //masyvas
     static int[] masyvas = new int[20];
 
     public static void main(String[] args) {
@@ -20,6 +21,9 @@ public class Main {
         System.out.println("Suma: " + suma());
     }
 
+    /**
+     * Funkcija sukurianti random skaičius masyvui
+     */
     public static void random() {
         Random rand = new Random();
         for (int i = 0; i < 10; i++) {
@@ -30,70 +34,119 @@ public class Main {
         }
     }
 
+    /**
+     * Funkcija išvedanti masyva į ekraną
+     */
     public static void isvestiMasyva() {
+        System.out.print("Masyvas: ");
         for (int i = 0; i < 20; i++) {
             System.out.print(masyvas[i] + " ");
         }
         System.out.println();
     }
 
+    /**
+     * Funkcija suskaičiuojanti masyvo vidurkį
+     *
+     * @return gražinamas vidurkis
+     */
     public static double vidurkis() {
         double vidurkis = 0;
-        for (int i = 0;i<20;i++){
-            vidurkis = vidurkis+masyvas[i];
+        for (int i = 0; i < 20; i++) {
+            vidurkis = vidurkis + masyvas[i];
         }
-        vidurkis = vidurkis/20;
+        vidurkis = vidurkis / kiekis();
         return vidurkis;
     }
-    public static double suma(){
+
+    /**
+     * Funkcija suskaičioujanti masyvo suma
+     *
+     * @return gražinama suma
+     */
+    public static double suma() {
         double suma = 0;
-        for (int i = 0;i<20;i++){
-            suma = suma+masyvas[i];
+        for (int i = 0; i < 20; i++) {
+            suma = suma + masyvas[i];
         }
         return suma;
     }
-    public static int kiekis(){
+
+    /**
+     * Funkcija suskaičioujanti masyvo kiekį,ilgį
+     *
+     * @return gražinamas kiekis,ilgis
+     */
+    public static int kiekis() {
         return masyvas.length;
     }
-    public static void apkeisti(){
-        int x = min();
+
+    /**
+     * Funkcija apkeičianti mažiausia ir didžiausia masyvo reikšme
+     */
+    public static void apkeisti() {
+        int x = min();//laikinas kintamasis naudojamas apkeitimui
         masyvas[minIndex()] = max();
         masyvas[maxIndex()] = x;
     }
-    public static int min(){
+
+    /**
+     * Funkcija apskaičiuojanti mažiausia masyvo reikšme
+     *
+     * @return gražinama mažiausia masyvo reikšmė
+     */
+    public static int min() {
         int min = Integer.MAX_VALUE;
-        for(int i = 0;i<20;i++){
-            if (masyvas[i]<min){
+        for (int i = 0; i < 20; i++) {
+            if (masyvas[i] < min) {
                 min = masyvas[i];
             }
         }
         return min;
     }
-    public static int minIndex(){
+
+    /**
+     * Funkcija suskaičiuojanti mažiausios reikšmės indeksa masyve
+     *
+     * @return gražinamas mažiausios reikšmės indeksas masyve
+     */
+    public static int minIndex() {
         int min = Integer.MAX_VALUE;
         int minIndex = 0;
-        for(int i = 0;i<20;i++){
-            if (masyvas[i]<min){
+        for (int i = 0; i < 20; i++) {
+            if (masyvas[i] < min) {
                 min = masyvas[i];
                 minIndex = i;
             }
         }
         return minIndex;
     }
-    public static int max(){
+
+    /**
+     * Funkcija apskaičiuojanti didžiausia masyvo reikšme
+     *
+     * @return gražinama didžiausia masyvo reikšmė
+     */
+    public static int max() {
         int max = Integer.MIN_VALUE;
-        for(int i = 0;i<20;i++){
-            if (masyvas[i]>max){
+        for (int i = 0; i < 20; i++) {
+            if (masyvas[i] > max) {
                 max = masyvas[i];
             }
         }
         return max;
     }
-    public static int maxIndex(){
+
+    /**
+     * Funkcija suskaičiuojanti didžiausios reikšmės indeksa masyve
+     *
+     * @return gražinamas didžiausios reikšmės indeksas masyve
+     */
+    public static int maxIndex() {
         int max = Integer.MIN_VALUE;
         int maxIndex = 0;
-        for(int i = 0;i<20;i++){
-            if (masyvas[i]>max){
+        for (int i = 0; i < 20; i++) {
+            if (masyvas[i] > max) {
                 max = masyvas[i];
                 maxIndex = i;
             }
